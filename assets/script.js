9207dbc7afa6602d76011267aac1b74f0df32841
fazle <ryan7998@gmail.com>
2021-01-10 20:45:40 -0500

    localstorage developed

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,17 @@
-var tasks = [];
+//var tasks = [];
+loadTasks = function(){
+    var savedTasks = localStorage.getItem("scheduled");
+    savedTasks = JSON.parse(savedTasks);
+    if(savedTasks == null){
+        return [];
+    }else{
+        return savedTasks;
+    }
+}
+
+var tasks = loadTasks();
+//console.log(tasks);
+
 /* Show current date: */
 $("#currentDay").text(moment().format("LL"));
 
@@ -8,22 +21,29 @@ var ulList = $("<ul>")
     .addClass("list-group list-group-flush")
 
 $(".container").append(ulList);
-/* var liList = $('<li>').addClass("list-group-item");
-ulList.append(liList); */
+
 
 /* Create Time intervals: */
 var timeArr = ['09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM'];
 
-timeArr.forEach(el => {
+/* If local storage emptry: save time into array */
+if(tasks.length == 0){
+    for(var i=0; i<timeArr.length; i++){
+        tasks.push({
+            time : timeArr[i],
+            task : ''
+        })
+    }
+}
 
+for(var i = 0; i<tasks.length; i++){
     var liList = $('<li>').addClass("list-group-item row justify-content-around");
     var timeItem =$("<span>")
-        //.addClass('col-2')
-        .text(el);
+        .text(tasks[i].time);
     var taskItem = $("<input>")
         .attr("type", "text")
         .addClass("form-control")
-        //.addClass('col-8');
+        .val(tasks[i].task);
     var saveBtn = $("<button/>")
         .text("Save")
         .addClass("btn btn-primary");
@@ -31,15 +51,9 @@ timeArr.forEach(el => {
     liList.append(timeItem, taskItem, saveBtn);
     ulList.append(liList);
 
-    //console.log(moment().format("hh:mm"));
-    //console.log(el);
-
     var timeNow = moment().format('HH');
-    // var timeNow = 11;
-    var time = moment(el, 'LT').format('HH');
+    var time = moment(tasks[i].time, 'LT').format('HH');
     
-   /*  console.log(moment().format('HH'));
-    console.log(time.format('HH')); */
     if(timeNow>time){
         liList.addClass('list-group-item-dark');
     }else if(timeNow<time){
@@ -47,9 +61,8 @@ timeArr.forEach(el => {
     }else{
         liList.addClass('list-group-item-danger');
     }
-    // console.log(timeNow);
-    // console.log(time);
-});
+
+}
 
 $(".list-group").on("click", "button", function(){
     
@@ -69,51 +82,12 @@ $(".list-group").on("click", "button", function(){
 })
 
 saveTasks = function(timeInput, taskInput){
-    // console.log(timeInput, taskInput);
-
-//    tasks.push([timeInput, taskInput]);
-    /* tasks.forEach(el =>{
-        tasks[el]
-        
-    }); */
-// if nothing in localStorage, create a new object to track all task status arrays
-
-   
-    /* if(tasks.length == 0){
-        tasks.push({
-            time:timeInput,
-            task: taskInput
-        });
-    }else{
-        for(var i=0; i<tasks.length; i++){
-            if(tasks[i]['time'] == timeInput){
-                tasks[i]['task'] = taskInput;
-                console.log(tasks[i]['task']);
-            }else{
-                tasks.push({
-                    time: timeInput,
-                    task: taskInput
-                });
-                break;
-            }
+    for(var i=0; i<timeArr.length; i++){
+        if(tasks[i].time == timeInput){
+            tasks[i].task = taskInput;
         }
-    } */
-    
-    // if(timeInput in tasks){
-    //     tasks[timeInput] = [taskInput];
-    //     console.log(tasks);
-    // }else{
-    //     /* tasks.push({
-    //         time: timeInput,
-    //         task: taskInput
-    //     }); */
-    //     tasks[timeInput].push(taskInput);
-            
-    //     console.log(tasks);
-    // }
-    //console.dir(tasks);
-    
+    }
+    localStorage.setItem("scheduled", JSON.stringify(tasks));
 }
 
-loadTasks = function(){
-}
+
